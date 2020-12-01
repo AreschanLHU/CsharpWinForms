bfef48f05c56481a76558015f5a0477cea7c4ed4
AreschanLHU <59295456+AreschanLHU@users.noreply.github.com>
2020-12-02 01:55:15 +0800

    Day1-20201201

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            x = 1;
+            x = 2;
+            x = x + 1;
+            Console.WriteLine(x);
+            Console.ReadLine();
+        }
+    }
+}
